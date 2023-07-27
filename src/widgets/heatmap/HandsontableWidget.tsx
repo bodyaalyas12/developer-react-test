c00d9efb98d7bbc85aa91e1ae33c2fdd18644afd
Bogdan Kolesnychenko <bogdan.kolesnychenko@cprime.com>
2023-07-27 19:30:10 +0200

    minor fixes

--- src/widgets/heatmap/HandsontableWidget.tsx
+++ src/widgets/heatmap/HandsontableWidget.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useCallback, useMemo } from "react";
 import { Box } from "@mui/material";
 import { HeatmapProps } from "./Heatmap.props";
 import "handsontable/dist/handsontable.full.min.css";
@@ -8,37 +8,34 @@ import { registerRenderer, textRenderer } from "handsontable/renderers";
 import { BaseRenderer } from "handsontable/renderers/base";
 import chroma from "chroma-js";
 import { getMax, getMin } from "../../utils/minMax";
+import { CellMeta } from "handsontable/settings";
 
 registerAllModules();
 
 export const HandsontableWidget = (props: HeatmapProps) => {
   const { tableData, tableHeaders } = props;
 
-  const { min, max } = useMemo(() => {
+  const scale = useMemo(() => {
     const numbers = tableData
       .flat()
       .filter((item) => typeof item === "number") as number[];
-    return { min: getMin(numbers), max: getMax(numbers) };
+    const min = getMin(numbers);
+    const max = getMax(numbers);
+    return chroma.scale("RdYlBu").domain([max, min]);
   }, [tableData]);
 
-  const scale = chroma.scale("RdYlBu").domain([max, min]);
+  const heatMapRenderer: BaseRenderer = useCallback(
+    function (instance, td, row, col, prop, value, cellProperties) {
+      textRenderer(instance, td, row, col, prop, value, cellProperties);
+      if (typeof value !== "number") {
+        return;
+      }
+      const color = scale(value);
+      td.style.background = color.hex();
+    },
+    [scale],
+  );
 
-  const heatMapRenderer: BaseRenderer = function (
-    instance,
-    td,
-    row,
-    col,
-    prop,
-    value,
-    cellProperties,
-  ) {
-    textRenderer(instance, td, row, col, prop, value, cellProperties);
-    if (typeof value !== "number") {
-      return;
-    }
-    const color = scale(value as number);
-    td.style.background = color.hex();
-  };
   registerRenderer("heatMapRenderer", heatMapRenderer);
 
   return (
@@ -47,7 +44,7 @@ export const HandsontableWidget = (props: HeatmapProps) => {
         data={[tableHeaders, ...tableData]}
         licenseKey="non-commercial-and-evaluation"
         cells={(row, col, prop) => {
-          const cellProperties: any = {};
+          const cellProperties: CellMeta = {};
           if (row !== 0 && col !== 0) {
             cellProperties.renderer = "heatMapRenderer";
           }
